75f4009af5351172fd22e9b79ad75cbb85b4d151
um0unt <blueflasher666@gmail.com>
2019-03-24 16:05:37 +0300

    progect version 2

--- scaner.py
+++ scaner.py
@@ -1,13 +1,38 @@
+# -*- coding:utf-8 -*-
+
 import pandas as pd
 
-my_data = pd.read_csv('dump.csv', sep=',', header=0)                #читаем данные из csv
+get_df = pd.read_csv('dump.csv', sep=',', header=0)                		#read data from scv
 
-result_df = my_data.groupby(['Source','Protocol'])['No.'].count()   #делаем группировку
+tcp_df = pd.DataFrame(get_df[get_df['Protocol'] == 'TCP'])				#select only tcp form dump
 
-result_df = pd.DataFrame(result_df)                                 #Преобразуем обьект Series в DataFrames
+tcp_df.drop(['No.','Time','Length','Protocol'], axis=1, inplace=True)	#delete unnecessary columns
 
-result_df = result_df.rename(columns={'No.': 'Using count'})        #Меняем название столбца
+tcp_df['Info'].replace(													#regular expression for
+			to_replace='(\[((\w+\s)|[^\]])+\]\s*)|(\w+\=\d+)|\s+', 		#getting port numbers
+			value='', 													#from string Info
+			inplace=True, 												
+			regex=True
+			)
 
-print (result_df)                                                   #Выводим результат в консоль
+tcp_df = pd.concat(														#expand column Info on
+			[tcp_df,tcp_df['Info'].str.split('>', expand=True)], 		#two 0 and 1
+			axis=1
+			)
 
-result_df.to_csv('result.csv')                                      #Сохраняем результат в csv
+tcp_df = tcp_df.rename(columns={0:'Src name port', 1:'Dst name port'})	#rename columns 0 and 1
+
+tcp_df.drop(['Info'], axis=1, inplace=True)								#delete coloumn Info
+
+result = pd.DataFrame(tcp_df.groupby(									#create result table
+				['Source',
+				'Destination',
+				'Src name port',
+				'Dst name port'
+				])['Source'].size());
+
+result = result.rename(columns={'Source':'Number of requests'})			#rename sort colunm
+
+print(result)															#show dataframe in terminal
+
+result.to_csv('result.csv')                                      		#save result in file
